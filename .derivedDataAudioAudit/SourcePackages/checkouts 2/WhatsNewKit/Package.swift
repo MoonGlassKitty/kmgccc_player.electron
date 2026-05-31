0dae9fdafbf22e3ff532601cbae78092c53f756f
kmg <kmgccc@outlook.com>
2026-05-31 18:02:48 +0800

    Commit all current changes

---
+++ .derivedDataAudioAudit/SourcePackages/checkouts 2/WhatsNewKit/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "WhatsNewKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(
+            name: "WhatsNewKit",
+            targets: [
+                "WhatsNewKit"
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "WhatsNewKit",
+            path: "Sources",
+            resources: [
+                .process("Resources/PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .testTarget(
+            name: "WhatsNewKitTests",
+            dependencies: [
+                "WhatsNewKit"
+            ],
+            path: "Tests"
+        )
+    ]
+)
